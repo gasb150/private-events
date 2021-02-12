e2b3b75f9e93a28f7e59568191fde91f765544a6
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-02-13 00:21:14 +0300

    change the msg of form error

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,6 @@ class Event < ApplicationRecord
     has_many :attendances
     has_many :attendee , through: :attendances, source: :user
     has_many :creator , through: :attendances, class_name: "User"
-    
+
+    validates :title, presence: true    
 end
